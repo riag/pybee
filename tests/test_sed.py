3aebebb6d186335b4feb75219b68e5c3cfb68305
riag <riag@163.com>
2018-09-12 11:58:31 +0800

    fix bug

--- tests/test_sed.py
+++ tests/test_sed.py
@@ -61,3 +61,22 @@ ONBOOT=true
     assert match_list[1].group(1) == '00:0C:29:B5:24:43' 
     assert match_list[2].group(1) == '9837a1c3-6001-459d-95b3-ea04fe5ee9ab' 
 
+def test_replace_by_pattern(capsys):
+    
+    pybee.path.mkdir(test_dir, True)
+
+    p = os.path.join(test_dir, 'replace')
+    s = '''
+NETWORKING=yes
+HOSTNAME=test
+    '''
+    pybee.path.save_text_file(p, s)
+
+    pybee.sed.replace_by_pattern(p, 
+            r'(HOSTNAME)\s*=\s*.+', '\g<1>=mytest'
+            )
+    match = pybee.sed.find_by_pattern(
+            p, 'HOSTNAME\s*=\s*(.*)'
+            )
+    assert match != None
+    assert match.group(1) == 'mytest'
