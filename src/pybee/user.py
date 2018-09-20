2a521e4b3ad4aa1c444fee2ff08a212177e117a7
riag <riag@163.com>
2018-09-20 10:48:22 +0800

    加入 pybee.user 获取系统的用户信息模块

---
+++ src/pybee/user.py
@@ -0,0 +1,37 @@
+
+import os
+import pwd
+
+def get_puser_name():
+    '''
+    获取当前进程的用户名
+    '''
+    return pwd.getpwuid(os.getuid()).pw_name
+
+def get_puser_info():
+    '''
+    获取当前进程的用户信息
+    '''
+    return pwd.getpwuid(os.getuid())
+
+def get_login_name():
+    '''
+    获取当前登录的用户名
+    '''
+    return os.getlogin()
+
+def get_login_user_info():
+    '''
+    获取当前登录的用户信息
+    '''
+    return pwd.getpwnam(os.getlogin())
+
+def check_user_is_create(name):
+    try:
+        o = pwd.getpwname(name)
+        return True
+    except Exception:
+        return False
+    
+    return False
+
